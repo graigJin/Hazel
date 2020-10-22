1276188e8402ff2b5abb8010f26b7239764cce14
graigJin <zimmermann.email@web.de>
2020-10-22 19:03:16 +0200

    Setup basic application and entry point.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,26 @@
+#include "Application.h"
+
+namespace Hazel
+{
+	Hazel::Application::Application()
+	{
+
+	}
+
+	Hazel::Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		while (true)
+		{
+			
+		}
+	}
+
+
+}
+
+
